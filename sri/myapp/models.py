270e506bb11aa4f4d92360b1dcef5680f1f2afa8
plotswag <jeevaneshselvavinayagam@gmail.com>
2023-11-17 12:08:25 +0530

    Success

---
+++ sri/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib import admin
+class footballplayer (models.Model):
+    numofmatch=models.IntegerField()
+    name=models.CharField(max_length=100)
+    salary=models.IntegerField()
+    age=models.IntegerField()
+    height=models.IntegerField()
+
+class footballplayerAdmin(admin.ModelAdmin):
+    list_display=('numofmatch','name','salary','age','height')
